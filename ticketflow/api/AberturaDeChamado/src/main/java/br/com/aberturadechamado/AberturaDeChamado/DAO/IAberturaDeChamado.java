6e3dfc24d7a4531e89e84b9550656ab51865ea24
Yuri Gomes <126199410+Yuuhgod@users.noreply.github.com>
2024-06-03 10:08:27 -0300

    Add dashboard and API, update front-end

---
+++ ticketflow/api/AberturaDeChamado/src/main/java/br/com/aberturadechamado/AberturaDeChamado/DAO/IAberturaDeChamado.java
@@ -0,0 +1,9 @@
+package br.com.aberturadechamado.AberturaDeChamado.DAO;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.aberturadechamado.AberturaDeChamado.model.AberturaDeChamado;
+
+public interface IAberturaDeChamado extends CrudRepository<AberturaDeChamado, Integer>{
+
+}
