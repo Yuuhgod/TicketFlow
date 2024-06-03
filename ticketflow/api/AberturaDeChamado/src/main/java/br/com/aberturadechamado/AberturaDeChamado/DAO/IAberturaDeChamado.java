690d3fc185693470586f73a4b1557686f26a60d3
Yuri Gomes <126199410+Yuuhgod@users.noreply.github.com>
2024-06-03 19:43:59 -0300

    Delete ticketflow directory

--- ticketflow/api/AberturaDeChamado/src/main/java/br/com/aberturadechamado/AberturaDeChamado/DAO/IAberturaDeChamado.java
+++
@@ -1,9 +0,0 @@
-package br.com.aberturadechamado.AberturaDeChamado.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.aberturadechamado.AberturaDeChamado.model.AberturaDeChamado;
-
-public interface IAberturaDeChamado extends CrudRepository<AberturaDeChamado, Integer>{
-
-}
